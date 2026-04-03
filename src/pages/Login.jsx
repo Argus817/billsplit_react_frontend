26c982061fc2e074bb5a5b2d44745151e64ca281
Argus817 <namantandel@gmail.com>
2026-04-03 14:24:17 +0530

    Initial Changes

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginRegisterCommon from './LoginRegisterCommon'
+
+const Login = () => {
+    return (
+        <LoginRegisterCommon>
+            Login
+        </LoginRegisterCommon>
+    )
+}
+
+export default Login
